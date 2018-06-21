21be6f37a8613b2da5fdeb1773586dc5bd377f8c
Sean Oughton <seanoughton@gmail.com>
2018-06-21 23:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
-function findMatching(drivers, name) {
+function findMatching(drivers, string) {
   return drivers.filter(
-    function (something){return something === name}
+    function (name){return name === string}
   );
 }
 
