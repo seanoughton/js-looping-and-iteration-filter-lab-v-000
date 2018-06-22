35b4e2b8078960ea1e508afdaa6436d2b0483241
Sean Oughton <seanoughton@gmail.com>
2018-06-22 00:01:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function fuzzyMatch(drivers,string){
 
 function matchName(drivers,string){
   return drivers.filter(
-    function (object["name"]){
-      
+    function (object){
+
     }
   );
 }
